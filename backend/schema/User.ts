716c0f330226fe6105733f4f5de931db59225406
UP210052 <up210052@alumnos.upa.edu.mx>
2024-10-30 18:18:04 -0600

    Changes in documentation and database

--- backend/schema/User.ts
+++ backend/schema/User.ts
@@ -1,5 +1,5 @@
 import { list } from '@keystone-6/core';
-import { integer, text, password, timestamp, relationship, float } from '@keystone-6/core/fields';
+import { integer, text, password, timestamp, relationship, checkbox } from '@keystone-6/core/fields';
 import { allowAll } from '@keystone-6/core/access';
 
 export const User = list({
@@ -9,6 +9,8 @@ export const User = list({
     email: text({ validation: { isRequired: true }, isIndexed: 'unique' }),
     password: password({ validation: { isRequired: true } }),
     phone: text(),
+    premiun_sucription: checkbox({ defaultValue: true }),
     creation_date: timestamp({ defaultValue: { kind: 'now' } }),
+    end_suscription_date: timestamp(),
   },
 });
