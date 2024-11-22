1cc7f77daedc5f8fb1115bd17c998500d98ccc35
UP210342 <up210342@alumnos.upa.edu.mx>
2024-11-21 18:37:48 -0600

    Premium screen

--- frontend/app/drawer/GoPremiumScreen.tsx
+++ frontend/app/drawer/GoPremiumScreen.tsx
@@ -17,8 +17,12 @@ const GoPremiumScreen = () => {
   const renderBenefitItem = ({ item }: { item: typeof benefitsData[0] }) => (
     <View style={styles.row}>
       <Text style={styles.featureText}>{item.feature}</Text>
-      <Text style={styles.checkmark}>{item.free ? '✔️' : '—'}</Text>
-      <Text style={styles.checkmark}>{item.premium ? '✔️' : '—'}</Text>
+      <Text style={styles.checkmark}>
+        {item.free ? <Ionicons name="checkmark" size={20} color="#444444" /> : '—'}
+      </Text>
+      <Text style={styles.checkmark}>
+        {item.premium ? <Ionicons name="checkmark" size={20} color="#444444" /> : '—'}
+      </Text>
     </View>
   );
 
@@ -53,6 +57,10 @@ const GoPremiumScreen = () => {
           showsVerticalScrollIndicator={false}
         />
       </View>
+      {/* Purchase Button */}
+      <TouchableOpacity style={styles.purchaseButton}>
+        <Text style={styles.purchaseButtonText}>Purchase</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -70,14 +78,14 @@ const styles = StyleSheet.create({
     zIndex: 10,
   },
   introText: {
-    marginTop: 80,
+    marginTop: 20,
     fontSize: 16,
     fontWeight: 'bold',
     color: '#333',
-    textAlign: 'justify', // Add this line
-    textAlignVertical: 'center', // Optional, to align vertically
-    marginBottom: 1,
-    paddingHorizontal: 15
+    textAlign: 'justify',
+    textAlignVertical: 'center',
+    marginBottom: 10,
+    paddingHorizontal: 15,
   },
   headerContainer: {
     paddingHorizontal: 10,
@@ -88,7 +96,8 @@ const styles = StyleSheet.create({
     borderBottomWidth: 2,
     borderBottomColor: 'rgba(0, 0, 0, 0.2)',
     paddingBottom: 40,
-    marginBottom: 10,
+    marginBottom: 1,
+    marginTop: 80,
   },
   headerText: {
     fontSize: 16,
@@ -101,8 +110,9 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     paddingHorizontal: 5,
     marginVertical: 10,
-    height: 520,
+    height: 330,
     overflow: 'hidden',
+    marginTop: 10,
   },
   tableHeader: {
     flexDirection: 'row',
@@ -137,6 +147,22 @@ const styles = StyleSheet.create({
     flex: 1,
     fontSize: 14,
     textAlign: 'center',
+    color: '#444444',
+  },
+  // Style for the Purchase Button
+  purchaseButton: {
+    backgroundColor: '#3D8AF7',
+    paddingVertical: 15,  // Vertical padding
+    marginHorizontal: 125,
+    marginTop: 20,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  purchaseButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
